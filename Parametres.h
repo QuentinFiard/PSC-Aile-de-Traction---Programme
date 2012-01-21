afc00abcd3c993c1cb13bd614a7e7fa41dfd06ba
Quentin Fiard <quentin.fiard@polytechnique.org>
2012-01-21 19:06:22 +0100

    Ajout d'un fichier de paramètres du projet (propriétés de l'aile, constantes...)

---
+++ Parametres.h
@@ -0,0 +1,30 @@
+//
+//  Parametres.h
+//  PSC
+//
+//  Created by Quentin Fiard on 20/12/11.
+//  Copyright (c) 2011 École Polytechnique. All rights reserved.
+//
+
+#ifndef PSC_Parametres_h
+#define PSC_Parametres_h
+
+// Paramètre de l'aile
+
+#define MASSEAILE 0.5
+
+#define Ixx 0
+#define Iyy 0
+#define Izz 0
+
+//Test 2D, les valeurs sont en degrés
+#define GAMMA 10
+
+#define theta0 20
+#define thetapt0 0
+
+#define LONGUEURCABLES 30
+
+typedef double Reel; // changer pour modifier la précision des calculs
+
+#endif
