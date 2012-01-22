e1eb4c8bffa1c6632671a52804d68ca63ca39a16
Quentin Fiard <quentin.fiard@polytechnique.org>
2012-01-22 16:08:20 +0100

    Création de la classe UI, gérant l'interface utilisateur

---
+++ UI/UIController.h
@@ -0,0 +1,13 @@
+//
+//  UIController.h
+//  PSC
+//
+//  Created by Quentin Fiard on 22/01/12.
+//  Copyright (c) 2012 École Polytechnique. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UIController : NSViewController
+
+@end
---
+++ UI/UIController.m
@@ -0,0 +1,23 @@
+//
+//  UIController.m
+//  PSC
+//
+//  Created by Quentin Fiard on 22/01/12.
+//  Copyright (c) 2012 École Polytechnique. All rights reserved.
+//
+
+#import "UIController.h"
+
+@implementation UIController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
