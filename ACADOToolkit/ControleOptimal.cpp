20fa60ac2e5228b21885f5252d9d04cadd423e02
Quentin Fiard <quentin.fiard@polytechnique.org>
2012-02-11 07:40:27 +0100

    Ajout de la bibliothèque ACADOToolkit, test de compilation

---
+++ ACADOToolkit/ControleOptimal.cpp
@@ -0,0 +1,9 @@
+//
+//  ControleOptimal.cpp
+//  PSC
+//
+//  Created by Quentin Fiard on 09/02/12.
+//  Copyright (c) 2012 École Polytechnique. All rights reserved.
+//
+
+#include "ControleOptimal.h"
---
+++ ACADOToolkit/ControleOptimal.h
@@ -0,0 +1,20 @@
+//
+//  ControleOptimal.h
+//  PSC
+//
+//  Created by Quentin Fiard on 09/02/12.
+//  Copyright (c) 2012 École Polytechnique. All rights reserved.
+//
+
+#ifndef PSC_ControleOptimal_h
+#define PSC_ControleOptimal_h
+
+#include <acado_toolkit.hpp>
+
+class ControleOptimal
+{
+	
+};
+
+
+#endif
